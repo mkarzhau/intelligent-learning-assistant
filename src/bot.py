a01a87db0003dac56d553e7d53d14333453f6b46
maxsikxx <231324@astanait.edu.kz>
2025-11-02 01:14:54 +0500

    edit bot py

--- src/bot.py
+++ src/bot.py
@@ -1,17 +1,14 @@
 import asyncio
 import datetime
 import logging
-from sched import scheduler
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties # 1. Импортируем новый класс
+from aiogram.client.default import DefaultBotProperties
 from motor.motor_asyncio import AsyncIOMotorClient
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from . import config
-from .handlers import common #, quiz, schedule # Раскомментируйте, когда создадите эти файлы
+import config
+from handlers import common, schedule
 
-from .handlers import common, schedule 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
 async def motivation_scheduler(bot, db):
@@ -19,16 +16,14 @@ async def motivation_scheduler(bot, db):
         now = datetime.datetime.now(common.LOCAL_TZ)
         if now.hour == 9 and now.minute == 0:
             await common.send_motivation_to_all(bot, db)
-            await asyncio.sleep(60)  # чтобы не отправлять несколько раз за минуту
-        await asyncio.sleep(30)  # проверять каждые 30 секунд
+            await asyncio.sleep(60)
+        await asyncio.sleep(30)
 
 async def main():
-    # --- 1. Настройка подключений ---
     mongo_client = AsyncIOMotorClient(config.MONGO_URL)
     db = mongo_client[config.MONGO_DB_NAME]
     scheduler = AsyncIOScheduler(timezone="Asia/Almaty")
 
-    # --- 2. Инициализация бота ---
     default_properties = DefaultBotProperties(parse_mode="HTML")
     bot = Bot(token=config.BOT_TOKEN, default=default_properties)
     dp = Dispatcher()
@@ -37,19 +32,16 @@ async def main():
     dp.workflow_data['scheduler'] = scheduler
     dp.workflow_data['bot'] = bot
 
-    # --- 3. Регистрация роутеров ---
     dp.include_router(common.router)
     # dp.include_router(quiz.router)
     # dp.include_router(schedule.router)
 
-    # --- 4. Запуск фоновых задач ---
     scheduler.add_job(
         schedule.send_daily_blocks_and_reminder,
         'cron',
         minute=0,
         args=(bot, db)
     )
-    # Добавь задачу мотивации:
     scheduler.add_job(
         common.send_motivation_to_all,
         'cron',
@@ -59,7 +51,6 @@ async def main():
     )
     scheduler.start()
 
-    # --- 5. Запуск бота ---
     await bot.delete_webhook(drop_pending_updates=True)
     logging.info("Starting bot...")
     await dp.start_polling(bot)
